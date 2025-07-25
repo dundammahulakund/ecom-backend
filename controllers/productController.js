051e9c33ca815ca79ba833567d11084696b74da4
klebcabailhongal <dundammahulakund@gmail.com>
2025-07-25 10:41:24 +0530

    Product CRUD

---
+++ controllers/productController.js
@@ -0,0 +1,106 @@
+const Product = require("../models/Product");
+
+const getAllProducts = async (req, res) => {
+  try {
+    const product = await Product.find();
+
+    res.json(products);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+};
+const getproductById = async (req, res) => {
+  try {
+    const productId = req.params.id;
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "product not found",
+      });
+    }
+
+    res.json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+};
+
+const createproduct = async (req, res) => {
+  try {
+    const productData = req.body;
+    const newproduct = await Product.create(productData);
+
+    res.status(201).json({
+      message: "product created",
+      product: newproduct,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+};
+const updateproduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const productData = req.body;
+
+    const newproduct = await Product.findByIdAndUpdate(productId, productData, {
+      new: true,
+    });
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+    res.json({
+      message: "product updated",
+      product,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+};
+
+const deleteproduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const newproduct = await Product.findByIdAndUpdate(productId);
+
+    if (!product) {
+      return res.status(204).json({
+        message: "Product not found",
+      });
+    }
+    res.json({
+      message: "product delete",
+      product,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getproductById,
+  createproduct,
+  updateproduct,
+  deleteproduct,
+};
