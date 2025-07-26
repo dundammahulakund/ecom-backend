467247d6854afd74db852ff92a09145b4979fb3d
klebcabailhongal <dundammahulakund@gmail.com>
2025-07-26 16:08:25 +0530

    cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -55,6 +55,33 @@ const addToCart = async (req, res, next) => {
   }
 };
 
+const clearCookieCart = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+
+    const cart = await Cart.findOne({ userId });
+    if (!cart) {
+      return res.status(404).json({
+        message: "Cart not found",
+      });
+    }
+
+    cart.products = [];
+    cart.totalPrice = 0;
+    await cart.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Cart cleared successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+};
+
 async function calculateTotalPrice(products) {
   let total = 0;
   for (const item of products) {
